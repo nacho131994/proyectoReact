a655f78c2e320deb30728bce622a916f025eb53a
meryvl <meryvalerolaguna@gmail.com>
2023-02-23 13:17:26 +0000

    my commit

--- src/js/component/Cards.jsx
+++ src/js/component/Cards.jsx
@@ -6,47 +6,47 @@ const Cards = ()=>{
     return (
         <>
         <div className= "container">
-       <div class="row col-12 my-4">
-  <div class="col-3">
+       <div class="row my-4">
+       <div class="col-3">
     <div class="card">
-      <img src="..." class="card-img-top" alt="..." />
+      <img src="..." class="card-img-top" alt="..."  />
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipiscing elit. Sapiente esse necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Find Out More</a>
       </div>
     </div>
   </div>
 
   <div class="col-3">
     <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+      <img src="..." class="card-img-top" alt="..."  />
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipiscing elit. Sapiente esse necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Find Out More</a>
       </div>
     </div>
   </div>
 
   <div class="col-3">
     <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+      <img src="..." class="card-img-top" alt="..."  />
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipiscing elit. Sapiente esse necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Find Out More</a>
       </div>
     </div>
   </div>
 
-  <div class="col-3">
+    <div class="col-3">
     <div class="card">
-    <img src="..." class="card-img-top" alt="..."/>
+      <img src="..." class="card-img-top" alt="..."  />
       <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Lorem ipsum dolor sit amet,consectetur adipiscing elit. Sapiente esse necessitatibus neque.</p>
+        <a href="#" class="btn btn-primary">Find Out More</a>
       </div>
     </div>
   </div>
